adca51c40225b7b05e02818971c9e48be3f70d94
cichy380 <marcin.dobroszek@gmail.com>
2018-11-14 13:40:52 +0100

    Get posts and users by axios

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app" class="d-flex w-100 h-100 flex-column">
-    <header class="masthead">
+    <header class="masthead mb-4">
       <div class="inner">
         <h3 class="masthead-brand"><router-link to="/">Demo</router-link></h3>
         <nav class="nav nav-masthead justify-content-center">
@@ -11,7 +11,7 @@
 
     <router-view></router-view>
 
-    <footer class="mastfoot text-center">
+    <footer class="mastfoot mt-4 text-center">
       <div class="inner">
         <p>Cover template for <a href="https://getbootstrap.com/">Bootstrap</a>.</p>
       </div>
--- src/components/_posts.vue
+++ src/components/_posts.vue
@@ -1,28 +1,42 @@
 <template>
-    <main role="main" class="inner mb-auto bg-light text-dark shadow-none rounded my-4 p-3">
-        <h3>Posts</h3>
-        <div v-for="error in errors" :key="error" class="alert alert-danger mb-1">{{ error.message }}</div>
-        <p></p>
-        <p v-if="posts.length === 0">
-            <em>No posts.</em>
-        </p>
-        <p v-if="posts.length">
-            Loaded {{ posts.length }} posts.
-        </p>
+    <main role="main" class="inner mb-auto bg-light text-dark shadow-none rounded p-3">
+        <h3 class="mb-4">Posts <small class="text-muted tiny">({{ posts.length }} items)</small></h3>
+
+        <v-wait for="posts">
+            <template slot="waiting">
+                <p class="color-animation">
+                    Loading posts...
+                </p>
+            </template>
+        </v-wait>
+
+        <v-wait for="user.*">
+            <template slot="waiting">
+                <p class="color-animation">
+                    Loading users...
+                </p>
+            </template>
+        </v-wait>
+
+        <div v-for="error in errors" :key="error" class="alert alert-danger mb-1">{{ error }}</div>
+
         <p>
             <button v-if="posts.length === 0" @click="getAllPosts" class="btn btn-primary">Get posts</button>
             <button v-if="posts.length" @click="posts=[]" class="btn btn-danger">Remove posts</button>
         </p>
+
         <div class="list-group">
             <div v-for="(post, index) in posts"
                  :key="post.id"
-                 v-if="index <= 100"
+                 v-if="post.title && index <= 100"
                  class="list-group-item flex-column align-items-start">
                 <div class="d-flex w-100 justify-content-between">
                     <h5 class="mb-1">{{ post.title }}</h5>
                 </div>
                 <p class="mb-1">{{ post.body }}</p>
-                <small v-show="post.userName">Author: {{ post.userName }}</small>
+                <small>
+                    Author: {{ post.userName }} <em v-if="!post.userName" class="color-animation">loading...</em>
+                </small>
             </div>
         </div>
     </main>
@@ -43,29 +57,45 @@
             // Fetches all posts
             getAllPosts () {
                 let randomValue = Math.random()
+
+                // start waiting
+                this.$wait.start('posts')
+
                 axios.get(`http://jsonplaceholder.typicode.com/posts?rand=${randomValue}`)
-                    .then(response => {
+                    .then(response => { // handle success
                         response.data.forEach((post) => {
                             post.userName = ''
                             this.posts.push(post)
                         })
                         this.usersUpdate()
                     })
-                    .catch(e => {
+                    .catch(e => { // handle error
                         this.errors.push(e)
+                    })
+                    .then(() => { // always executed
+                        // stop waiting
+                        this.$wait.end('posts')
                     })
             },
 
             usersUpdate () {
                 this.posts.forEach((post) => {
                     if (post.userId) {
-                        axios.get(`https://jsonplaceholder.typicode.com/users/${post.userId}`)
-                            .then(response => {
-                                this.users.push(response.data)
+                        let randomValue = Math.random()
+
+                        // start waiting
+                        this.$wait.start(`user.${post.userId}-${randomValue}`)
+
+                        axios.get(`https://jsonplaceholder.typicode.com/users/${post.userId}?rand=${randomValue}`)
+                            .then(response => { // handle success
                                 post.userName = response.data.name
                             })
-                            .catch(e => {
-                                this.errors.push(e)
+                            .catch(e => { // handle error
+                                this.errors.push(`ERROR while get User (id: ${post.userId})<br>Message: ${e.message}`)
+                            })
+                            .then(() => { // always executed
+                                // stop waiting
+                                this.$wait.end(`user.${post.userId}-${randomValue}`)
                             })
                     }
                 })
